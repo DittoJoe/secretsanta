3f8e72253da6e6d5eaed742155c80505b29533a5
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-10-15 16:10:59 +0200

    it now generates two pairs of lists for family and partners

--- santa.rb
+++ santa.rb
@@ -1,2 +1,33 @@
-participants = %w[Dad Jenny Joe Katie Mum Sam]
-p participants.sample
+emails = %w[len.rohde@bt.com rohdejenny@gmail.com joseph.jf.rohde@gmail.com kateinaus@gmail.com itsmechrisrohde@gmail.com 1989sam.r@gmail.com]
+givers = %w[Dad Jenny Joe Katie Mum Sam]
+partners = %w[Daniel Viktor Rob Kat]
+takers = givers.shuffle
+i = 0
+
+while i < takers.length
+  if givers[i] == takers[i]
+    takers = givers.shuffle
+    i = 0
+  end
+  i += 1
+end
+
+p givers
+p takers
+
+givers.delete('Mum')
+givers.delete('Dad')
+
+takers = partners.shuffle
+i = 0
+
+while i < takers.length
+  if partners[i] == takers[i]
+    takers = partners.shuffle
+    i = 0
+  end
+  i += 1
+end
+
+p givers
+p takers
