abaddac7d8d4248e485229ada9a418a2e4b4ffaf
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-10-19 15:07:47 +0200

    creating an interation to send emails to all participlants

--- santa.rb
+++ santa.rb
@@ -1,16 +1,21 @@
 require 'pony'
 
-emails = %w[len.rohde@bt.com
-            rohdejenny@gmail.com
-            joseph.jf.rohde@gmail.com
-            kateinaus@gmail.com
-            itsmechrisrohde@gmail.com
-            1989sam.r@gmail.com]
-givers = %w[Dad Jenny Joe Katie Mum Sam]
+# emails = %w[rohdejenny@gmail.com
+#             joseph.jf.rohde@gmail.com
+#             kateinaus@gmail.com
+#             1989sam.r@gmail.com
+#             len.rohde@bt.com
+#             itsmechrisrohde@gmail.com]
+emails = []
+6.times { emails << "joseph.jf.rohde@gmail.com" }
+
+givers = %w[Jenny Joe Katie Sam Dad Mum]
+givers2 = %w[Jenny Joe Katie Sam]
 partners = %w[Daniel Viktor Rob Cat]
 takers = givers.shuffle
-i = 0
+takers2 = partners.shuffle
 
+i = 0
 while i < takers.length
   if givers[i] == takers[i]
     takers = givers.shuffle
@@ -22,13 +27,7 @@ end
 p givers
 p takers
 
-givers2 = givers
-givers2.delete('Mum')
-givers2.delete('Dad')
-
-takers2 = partners.shuffle
 i = 0
-
 while i < takers2.length
   if partners[i] == takers2[i]
     takers2 = partners.shuffle
@@ -40,4 +39,17 @@ end
 p givers2
 p takers2
 
-Pony.mail(:to => emails[2], :body => "You get to buy for #{takers[2]} and #{takers2[2]}")
+# Pony.mail(:to => emails[1],
+#           :from => 'santa@lapland.com',
+#           :subject => 'Your Secret Santa names',
+#           :body => "Hi #{givers[1]}! You're buying for #{takers[1]} and #{takers2[1]}. Love, Santa"
+#           )
+
+# i = 0
+# while i < givers.length
+#   Pony.mail(:to => emails[i],
+#             :from => 'santa@lapland.com',
+#             :subject => 'Your Secret Santa names',
+#             :body => "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa")
+#   i +=1
+# end
