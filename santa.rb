65caac4a442c51163576492d5d33d9e8c8f28676
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-10-20 16:59:40 +0200

    added puts statement to show how the emails should look

--- santa.rb
+++ santa.rb
@@ -1,13 +1,13 @@
 require 'pony'
 
-# emails = %w[rohdejenny@gmail.com
-#             joseph.jf.rohde@gmail.com
-#             kateinaus@gmail.com
-#             1989sam.r@gmail.com
-#             len.rohde@bt.com
-#             itsmechrisrohde@gmail.com]
-emails = []
-6.times { emails << "joseph.jf.rohde@gmail.com" }
+emails = %w[rohdejenny@gmail.com
+            joseph.jf.rohde@gmail.com
+            kateinaus@gmail.com
+            1989sam.r@gmail.com
+            len.rohde@bt.com
+            itsmechrisrohde@gmail.com]
+# emails = []
+# 6.times { emails << "joseph.jf.rohde@gmail.com" }
 
 givers = %w[Jenny Joe Katie Sam Dad Mum]
 givers2 = %w[Jenny Joe Katie Sam]
@@ -24,8 +24,8 @@ while i < takers.length
   i += 1
 end
 
-p givers
-p takers
+# p givers
+# p takers
 
 i = 0
 while i < takers2.length
@@ -36,8 +36,8 @@ while i < takers2.length
   i += 1
 end
 
-p givers2
-p takers2
+# p givers2
+# p takers2
 
 # Pony.mail(:to => emails[1],
 #           :from => 'santa@lapland.com',
@@ -45,11 +45,13 @@ p takers2
 #           :body => "Hi #{givers[1]}! You're buying for #{takers[1]} and #{takers2[1]}. Love, Santa"
 #           )
 
-# i = 0
-# while i < givers.length
-#   Pony.mail(:to => emails[i],
-#             :from => 'santa@lapland.com',
-#             :subject => 'Your Secret Santa names',
-#             :body => "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa")
-#   i +=1
-# end
+i = 0
+while i < givers.length
+  # Pony.mail(:to => emails[i],
+  #           :from => 'santa@lapland.com',
+  #           :subject => 'Your Secret Santa names',
+  #           :body => "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa")
+  puts emails[i]
+  puts "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa"
+  i +=1
+end
