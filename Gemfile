39032a38873d3ad0a467ffe44c28b4236f59cee3
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-10-21 11:59:32 +0200

    implementing sendgrid for more reliable email sends

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 gem 'dotenv', groups: [:development, :test]
 gem 'pry'
 gem 'pony', '~> 1.11'
+gem 'sendgrid-ruby'
--- santa.rb
+++ santa.rb
@@ -1,13 +1,10 @@
-require 'pony'
+require 'sendgrid-ruby'
+include SendGrid
 
-emails = %w[rohdejenny@gmail.com
-            joseph.jf.rohde@gmail.com
-            kateinaus@gmail.com
-            1989sam.r@gmail.com
-            len.rohde@bt.com
-            itsmechrisrohde@gmail.com]
-# emails = []
-# 6.times { emails << "joseph.jf.rohde@gmail.com" }
+# emails = %w[rohdejenny@gmail.com joseph.jf.rohde@gmail.com kateinaus@gmail.com
+#             1989sam.r@gmail.com len.rohde@bt.com itsmechrisrohde@gmail.com]
+emails = []
+6.times { emails << "joseph.jf.rohde@gmail.com" }
 
 givers = %w[Jenny Joe Katie Sam Dad Mum]
 givers2 = %w[Jenny Joe Katie Sam]
@@ -24,9 +21,6 @@ while i < takers.length
   i += 1
 end
 
-# p givers
-# p takers
-
 i = 0
 while i < takers2.length
   if partners[i] == takers2[i]
@@ -36,22 +30,17 @@ while i < takers2.length
   i += 1
 end
 
-# p givers2
-# p takers2
-
-# Pony.mail(:to => emails[1],
-#           :from => 'santa@lapland.com',
-#           :subject => 'Your Secret Santa names',
-#           :body => "Hi #{givers[1]}! You're buying for #{takers[1]} and #{takers2[1]}. Love, Santa"
-#           )
-
 i = 0
 while i < givers.length
-  # Pony.mail(:to => emails[i],
-  #           :from => 'santa@lapland.com',
-  #           :subject => 'Your Secret Santa names',
-  #           :body => "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa")
+  # from = Email.new(email: 'joseph.jf.rohde@gmail.com')
+  # to = Email.new(email: emails[i])
+  # subject = 'Secret Santa Names'
+  # content = Content.new(type: 'text/plain', value: "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa")
+  # mail = Mail.new(from, subject, to, content)
+  # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  # response = sg.client.mail._('send').post(request_body: mail.to_json)
   puts emails[i]
   puts "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa"
   i +=1
 end
+
