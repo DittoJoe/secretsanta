4d6e394be0003c1101f4700b26d8294a65936449
Joseph Rohde <joseph.jf.rohde@gmail.com>
2021-10-25 16:06:11 +0200

    names are ready to generate

--- santa.rb
+++ santa.rb
@@ -3,52 +3,41 @@ require 'dotenv'
 Dotenv.load
 include SendGrid
 
-p ENV['SENDGRID_API_KEY']
-p ENV['EMAIL']
+def santa_shuffle(array)
+  i = 0
+  result = array
+  while i < array.length
+    if array[i] == result[i]
+      result = array.shuffle
+      i = 0
+    end
+    i += 1
+  end
+  return result
+end
 
 emails = [ENV['EMAIL_JENNY'], ENV['EMAIL_JOE'], ENV['EMAIL_KATIE'],
           ENV['EMAIL_SAM'], ENV['EMAIL_DAD'], ENV['EMAIL_MUM']]
-
-# emails = []
-# 6.times { emails << ENV['EMAIL_JOE'] }
-
-p emails
-
 givers = %w[Jenny Joe Katie Sam Dad Mum]
-givers2 = %w[Jenny Joe Katie Sam]
 partners = %w[Daniel Viktor Rob Cat]
-takers = givers.shuffle
-takers2 = partners.shuffle
+takers = santa_shuffle(givers)
+takers2 = santa_shuffle(partners)
 
-i = 0
-while i < takers.length
-  if givers[i] == takers[i]
-    takers = givers.shuffle
-    i = 0
-  end
-  i += 1
-end
-
-i = 0
-while i < takers2.length
-  if partners[i] == takers2[i]
-    takers2 = partners.shuffle
-    i = 0
-  end
-  i += 1
-end
+puts 'Names generated!'
 
 i = 0
 while i < givers.length
-  # from = Email.new(email: 'joseph.jf.rohde@gmail.com')
-  # to = Email.new(email: emails[i])
-  # subject = 'Secret Santa Names'
-  # content = Content.new(type: 'text/plain', value: "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa")
-  # mail = Mail.new(from, subject, to, content)
-  # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-  # response = sg.client.mail._('send').post(request_body: mail.to_json)
-  # puts emails[i]
-  # puts "Hi #{givers[i]}! You're buying for #{takers[i]}#{" and #{takers2[i]}" if i < 4}. Love, Santa"
+  takers_string = "#{takers[i]}#{" and #{takers2[i]}" if i < 4}"
+  from = Email.new(email: ENV['EMAIL_JOE'])
+  to = Email.new(email: emails[i])
+  subject = 'Secret Santa Names'
+  content = Content.new(type: 'text/plain',
+                        value: "Hi #{givers[i]}! You're buying for #{takers_string}. Love, Santa")
+  mail = Mail.new(from, subject, to, content)
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts "Email sent to #{givers[i]}!" if response.status_code == '202'
   i +=1
 end
 
+puts 'Merry Christmas!'
